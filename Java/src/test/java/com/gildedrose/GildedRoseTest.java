8ec0d605eba47321eccfc7f4444afc8044e9e100
Sander Steenhoudt <sander.steenhoudt@protonmail.com>
2023-11-23 15:59:13 +0100

    Added tests checking existing requirements

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,137 @@
 package com.gildedrose;
 
+import com.gildedrose.testfixtures.ItemMother;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.gildedrose.assertions.ItemAssertions.assertThat;
 
+//TODO make more generic
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
+    void shouldSetItems() {
+        Item vest = ItemMother.dexterityVest();
+        GildedRose app = new GildedRose(new Item[]{vest});
+        Assertions.assertThat(app.items[0]).isEqualTo(vest);
+    }
+
+    private Item startAppAndGetUpdatedItem(Item item) {
+        GildedRose app = new GildedRose(new Item[]{item});
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+        return app.items[0];
+    }
+
+    /**
+     * Depreciating items lose quality over time.
+     */
+    @Nested
+    class DepreciatingItems {
+
+        @Test
+        void shouldDecrementSellInDaysByOneWhenUpdatingQuality() {
+            Item updatedVest = startAppAndGetUpdatedItem(ItemMother.dexterityVest());
+            assertThat(updatedVest).hasDaysLeftToSell(ItemMother.VEST_SELL_IN_DAYS - 1);
+        }
+
+        @Test
+        void shouldDecrementQualityByOneWhenSellByDateNotReached() {
+            Item vest = ItemMother.dexterityVest();
+            vest.sellIn = 5;
+            Item updatedVest = startAppAndGetUpdatedItem(vest);
+            assertThat(updatedVest).hasQuality(ItemMother.VEST_QUALITY - 1);
+        }
+
+        @Test
+        void shouldDecrementQualityByTwoWhenSellByDateReached() {
+            Item vest = ItemMother.dexterityVest();
+            vest.sellIn = 0;
+            Item updatedVest = startAppAndGetUpdatedItem(vest);
+            assertThat(updatedVest).hasQuality(ItemMother.VEST_QUALITY - 2);
+        }
+
+        @Test
+        void shouldDecrementQualityByTwoWhenSellByDateExceeded() {
+            Item vest = ItemMother.dexterityVest();
+            vest.sellIn = -1;
+            Item updatedVest = startAppAndGetUpdatedItem(vest);
+            assertThat(updatedVest).hasQuality(ItemMother.VEST_QUALITY - 2);
+        }
+
+    }
+
+    @Nested
+    class AppreciatingItems {
+        private static final int MAX_QUALITY = 50;
+
+        @Test
+        void shouldIncreaseInQuality() {
+            Item brie = startAppAndGetUpdatedItem(ItemMother.agedBrie());
+            assertThat(brie).hasQuality(ItemMother.AGED_BRIE_QUALITY + 1);
+        }
+
+        @Test
+        void qualityShouldBeCappedAtMaximum() {
+            Item brie = ItemMother.agedBrie();
+            brie.quality = MAX_QUALITY;
+            Item updatedVest = startAppAndGetUpdatedItem(brie);
+            assertThat(updatedVest).hasQuality(MAX_QUALITY);
+        }
+
+        //TODO use test factory with ranges to make more robust
+        @Nested
+        class BackstagePasses {
+
+            @Test
+            void shouldIncreaseQualityByOneWhenSellInDaysMoreThanTen() {
+                Item backstagePass = ItemMother.backstagePass();
+                backstagePass.sellIn = 11;
+                assertThat(startAppAndGetUpdatedItem(backstagePass)).hasQuality(ItemMother.BACKSTAGE_PASS_QUALITY + 1);
+            }
+
+            @Test
+            void shouldIncreaseQualityByTwoWhenSellInDaysBetweenTenAndFive() {
+                Item backstagePass = ItemMother.backstagePass();
+                backstagePass.sellIn = 9;
+                assertThat(startAppAndGetUpdatedItem(backstagePass)).hasQuality(ItemMother.BACKSTAGE_PASS_QUALITY + 2);
+            }
+
+            @Test
+            void shouldIncreaseQualityByThreeWhenSellInDaysLessThanFive() {
+                Item backstagePass = ItemMother.backstagePass();
+                backstagePass.sellIn = 4;
+                assertThat(startAppAndGetUpdatedItem(backstagePass)).hasQuality(ItemMother.BACKSTAGE_PASS_QUALITY + 3);
+            }
+
+            @Test
+            void shouldDropQualityToZeroWhenExpired() {
+                Item backstagePass = ItemMother.backstagePass();
+                backstagePass.sellIn = 0;
+                assertThat(startAppAndGetUpdatedItem(backstagePass)).hasQuality(0);
+            }
+        }
+    }
+
+    @Nested
+    class LegendaryItems {
+        private static final int LEGENDARY_QUALITY = 80;
+        private static final int LEGENDARY_SELL_IN_DAYS = -1;
+
+
+        @Test
+        void shouldNotChangeInQuality() {
+            Item sulfuras = startAppAndGetUpdatedItem(ItemMother.sulfuras());
+            assertThat(sulfuras).hasQuality(LEGENDARY_QUALITY);
+            assertThat(startAppAndGetUpdatedItem(sulfuras)).hasQuality(LEGENDARY_QUALITY);
+        }
+
+        @Test
+        void shouldNotChangeInSellInDays() {
+            Item sulfuras = startAppAndGetUpdatedItem(ItemMother.sulfuras());
+            assertThat(sulfuras).hasDaysLeftToSell(LEGENDARY_SELL_IN_DAYS);
+            assertThat(startAppAndGetUpdatedItem(sulfuras)).hasDaysLeftToSell(LEGENDARY_SELL_IN_DAYS);
+        }
     }
 
 }
--- Java/src/testFixtures/java/com/gildedrose/testfixtures/ItemMother.java
+++ Java/src/testFixtures/java/com/gildedrose/testfixtures/ItemMother.java
@@ -10,6 +10,13 @@ public class ItemMother {
     public static final String AGED_BRIE_NAME = "Aged Brie";
     public static final int AGED_BRIE_SELL_IN_DAYS = 2;
     public static final int AGED_BRIE_QUALITY = 0;
+    public static final String SULFURAS_NAME = "Sulfuras, Hand of Ragnaros";
+    public static final int SULFURAS_SELL_IN_DAYS = -1;
+    public static final int SULFURAS_QUALITY = 80;
+    public static final String BACKSTAGE_PASS_NAME = "Backstage passes to a TAFKAL80ETC concert";
+    public static final int BACKSTAGE_PASS_SELL_IN_DAYS = 15;
+    public static final int BACKSTAGE_PASS_QUALITY = 20;
+
 
     public static Item dexterityVest() {
         return new Item(VEST_NAME, VEST_SELL_IN_DAYS, VEST_QUALITY);
@@ -17,5 +24,13 @@ public class ItemMother {
 
     public static Item agedBrie() {
         return new Item(AGED_BRIE_NAME, AGED_BRIE_SELL_IN_DAYS, AGED_BRIE_QUALITY);
+    }
+
+    public static Item sulfuras() {
+        return new Item(SULFURAS_NAME, SULFURAS_SELL_IN_DAYS, SULFURAS_QUALITY);
+    }
+
+    public static Item backstagePass() {
+        return new Item(BACKSTAGE_PASS_NAME, BACKSTAGE_PASS_SELL_IN_DAYS, BACKSTAGE_PASS_QUALITY);
     }
 }
